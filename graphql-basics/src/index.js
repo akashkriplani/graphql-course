60fa3f22fb9572d71995bbe2baa43acfafaf4dd9
Akash <aakash23kriplani@gmail.com>
2022-03-09 19:52:13 +0530

    Assignment - Import export ES6

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,10 @@
 import myCurrentLocation, { name, message, getGreeting } from './myModule';
+import myAddFunction, { subtract } from './math';
 
 console.log(message);
 console.log(name);
 console.log(myCurrentLocation);
 console.log(getGreeting('Sachin'));
+
+console.log(myAddFunction(2, 1));
+console.log(subtract(2, 1));
