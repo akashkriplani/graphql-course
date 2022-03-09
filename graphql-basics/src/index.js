544555d21074202dc7b420fecb16bee52feeb63b
Akash <aakash23kriplani@gmail.com>
2022-03-09 20:32:21 +0530

    Add scalar types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,29 +1,35 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Scalar types - String, Boolean, Int, Float, ID
+
 // Type definitions (Schema)
 const typeDefs = `
   type Query {
-    hello: String!
+    id: ID!
     name: String!
-    location: String!
-    bio: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return `This is my first query!`;
+    id() {
+      return 'abc123';
     },
     name() {
       return `Akash Kriplani`;
     },
-    location() {
-      return `Lucknow`;
+    age() {
+      return 30;
     },
-    bio() {
-      return `Experience full stack web developer`;
+    employed() {
+      return true;
+    },
+    gpa() {
+      return 3.2;
     }
   }
 };
